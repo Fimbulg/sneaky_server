4c650cadcd9d86604a0804005d7448138289a524
Adam Olofsson <fimbulg@pop-os.localdomain>
2019-03-25 14:32:03 +0100

    add webhook

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express =  require('express');
 const bodyParser = require('body-parser');
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const shell = require('shelljs')
 
 const app = express();
 app.use(express.json());
@@ -11,7 +12,10 @@ let posts = [];
 app.get('/', (req, res) => {
     res.send('Helloror there world\n');
 });
-
+app.post('/repoUpdate', (req, res) => {
+    shell.exec('cd /home/fimbulg/Code/express/sneaky_server_compose_file && ' 
+    + 'docker-compose pull && docker-compose up --build');
+});
 app.post('/data', (req, res) => {
     console.log(req.body);
     posts.push(req.body);
